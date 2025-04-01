40045ea9266c36af5bac734393dd75dcd94f32ea
kaio-d-silva <kaio54220266@aluno.ms.senac.br>
2025-04-01 17:14:19 -0400

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import { Home, UserManagement } from "./Home";
+import { UserForm } from "./UserForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/users" element={<UserManagement />} />
+        <Route path="/users/new" element={<UserForm />} /> 
+        <Route path="/users/edit/:id" element={<UserForm isEditing />} />
+      </Routes>
+    </Router>
   );
 }
 
